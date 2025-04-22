8145aafbb17084fd8aaf7c94d7475b7553bd6692
PeytonElle <peytonpeters0403@gmail.com>
2025-04-22 11:22:35 -0500

    Added post feature (have to refresh)

--- static/js/app.js
+++ static/js/app.js
@@ -7,10 +7,22 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+    try{
+        const response = await fetch('/api/add_post', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                message, 
+            }), 
+        })
+    } catch (error) {
+        console.log('😭 Post failed', error)
+    }
 }
 
 window.onload =  async () => {
@@ -19,6 +31,6 @@ window.onload =  async () => {
         const posts = await response.json();
         posts.forEach((post) => renderPost(post));
     } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error('Error fetching posts:', error);
     }
 };
