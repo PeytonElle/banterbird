814ea6b7088ac001fcce822c1017a0715663e5ba
PeytonElle <peytonpeters0403@gmail.com>
2025-05-02 10:02:20 -0500

    rendering post every 5 seconds

--- static/js/app.js
+++ static/js/app.js
@@ -41,3 +41,14 @@ window.onload =  async () => {
         console.error('Error fetching posts:', error);
     }
 };
+
+setInterval(async() => {
+  try { 
+    const response = await fetch('/api/posts')
+    const posts = await response.json();
+    document.getElementById("feed").innerHTML = ""; // Clear the feed before rendering new post. 
+    posts.forEach((post) => renderPost(post));
+} catch (error) {
+    console.error('Error fetching posts:', error);
+}
+}, 5000) //5 seconds
